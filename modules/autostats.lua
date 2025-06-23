107908852d2a4d3031f869e9e50c75b4c441d3a5
hviet2510 <vieth1393@gmail.com>
2025-06-24 03:06:42 +0700

    Update autostats.lua

--- modules/autostats.lua
+++ modules/autostats.lua
@@ -2,9 +2,13 @@ return function()
 	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 	local player = game:GetService("Players").LocalPlayer
 
-	while task.wait(5) do
-		pcall(function()
-			ReplicatedStorage.Remotes.CommF_:InvokeServer("AddPoint", "Melee", 1)
-		end)
+	local function AutoAddStats()
+		while task.wait(3) do -- 3 giây 1 lần, giảm spam
+			pcall(function()
+				ReplicatedStorage.Remotes.CommF_:InvokeServer("AddPoint", "Melee", 1)
+			end)
+		end
 	end
+
+	task.spawn(AutoAddStats)
 end
