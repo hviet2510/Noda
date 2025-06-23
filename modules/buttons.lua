bf3fe2d26f33cdae6ab6285ae581ef283f8afe4f
hviet2510 <vieth1393@gmail.com>
2025-06-24 03:00:07 +0700

    Update buttons.lua

--- modules/buttons.lua
+++ modules/buttons.lua
@@ -1,22 +1,15 @@
 local Buttons = {}
 
 function Buttons.Create(parent, text, callback)
-	local Button = Instance.new("TextButton")
+	local Button = Instance.new("TextButton", parent)
 	Button.Size = UDim2.new(0, 200, 0, 30)
-	Button.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
 	Button.Text = text
+	Button.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
 	Button.TextColor3 = Color3.fromRGB(255, 255, 255)
 	Button.Font = Enum.Font.Gotham
 	Button.TextSize = 14
-	Button.Parent = parent
-
-	local UICorner = Instance.new("UICorner")
-	UICorner.CornerRadius = UDim.new(0, 6)
-	UICorner.Parent = Button
-
+	Instance.new("UICorner", Button).CornerRadius = UDim.new(0, 6)
 	Button.MouseButton1Click:Connect(callback)
-
-	return Button
 end
 
 return Buttons
