1c78eb992edcbffa717b87c0b768e6c8791856f2
hviet2510 <vieth1393@gmail.com>
2025-06-24 01:13:55 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,3 @@
--- Loader chính
 local EnemyList = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/enemylist.lua"))()
 local AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/autofarm.lua"))()
-local AutoStats = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/autostats.lua"))()
-local CreateUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/ui.lua"))()
-
-CreateUI(AutoFarm, AutoStats, EnemyList)
+AutoFarm(EnemyList)
