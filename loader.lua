1f17ec67b1cbd1a06894f08fe4fcc2352617a7cb
hviet2510 <vieth1393@gmail.com>
2025-06-24 01:39:50 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
-local EnemyList = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/enemylist.lua"))()
-local AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/autofarm.lua"))()
-local AutoStats = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/autostats.lua"))()
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/ui.lua"))()
+local EnemyList = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/EnemyList.lua"))()
+local AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/AutoFarm.lua"))()
+local AutoStats = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/AutoStats.lua"))()
 
 AutoFarm(EnemyList)
 AutoStats()
