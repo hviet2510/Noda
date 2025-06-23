14af881d43ae80a8b2b09b76a1fc398f12ba727f
hviet2510 <vieth1393@gmail.com>
2025-06-24 01:03:07 +0700

    Update enemylist.lua

--- enemylist.lua
+++ enemylist.lua
@@ -1,6 +1,42 @@
 return {
-	{Level = 1, Name = "Bandit", Quest = "BanditQuest1", Mob = "Bandit", Pos = Vector3.new(1058,16,1548)},
-	{Level = 10, Name = "Monkey", Quest = "JungleQuest", Mob = "Monkey", Pos = Vector3.new(-1599,37,153)},
-	{Level = 15, Name = "Gorilla", Quest = "JungleQuest", Mob = "Gorilla", Pos = Vector3.new(-1238,6,-486)},
-	-- Thêm đầy đủ danh sách Sea 1 như bản đã gửi trước
+    -- Starter Island
+    {Level = 1, Name = "Bandit", Quest = "BanditQuest1", Mob = "Bandit", Pos = Vector3.new(1058,16,1548)},
+    {Level = 10, Name = "Monkey", Quest = "JungleQuest", Mob = "Monkey", Pos = Vector3.new(-1599,37,153)},
+    {Level = 15, Name = "Gorilla", Quest = "JungleQuest", Mob = "Gorilla", Pos = Vector3.new(-1238,6,-486)},
+    {Level = 20, Name = "Pirate", Quest = "BuggyQuest1", Mob = "Pirate", Pos = Vector3.new(-1121,14,3894)},
+    {Level = 30, Name = "Brute", Quest = "BuggyQuest1", Mob = "Brute", Pos = Vector3.new(-1215,14,4305)},
+    {Level = 40, Name = "Desert Bandit", Quest = "DesertQuest", Mob = "Desert Bandit", Pos = Vector3.new(932,7,4484)},
+    {Level = 60, Name = "Desert Officer", Quest = "DesertQuest", Mob = "Desert Officer", Pos = Vector3.new(1572,10,4373)},
+    
+    -- More Islands
+    {Level = 75, Name = "Snow Bandit", Quest = "SnowQuest", Mob = "Snow Bandit", Pos = Vector3.new(1289,87,-1384)},
+    {Level = 85, Name = "Snowman", Quest = "SnowQuest", Mob = "Snowman", Pos = Vector3.new(1424,55,-1327)},
+    {Level = 90, Name = "Chief Petty Officer", Quest = "MarineQuest2", Mob = "Chief Petty Officer", Pos = Vector3.new(-4855,23,4308)},
+    {Level = 100, Name = "Sky Bandit", Quest = "SkyQuest", Mob = "Sky Bandit", Pos = Vector3.new(-4961,278,3750)},
+    {Level = 110, Name = "Dark Master", Quest = "SkyQuest", Mob = "Dark Master", Pos = Vector3.new(-5252,389,3897)},
+    {Level = 120, Name = "Prisoner", Quest = "PrisonerQuest", Mob = "Prisoner", Pos = Vector3.new(5138,0,474)},
+    {Level = 130, Name = "Dangerous Prisoner", Quest = "PrisonerQuest", Mob = "Dangerous Prisoner", Pos = Vector3.new(5098,0,802)},
+    {Level = 140, Name = "Toga Warrior", Quest = "ColosseumQuest", Mob = "Toga Warrior", Pos = Vector3.new(-1576,8,-2986)},
+    {Level = 160, Name = "Gladiator", Quest = "ColosseumQuest", Mob = "Gladiator", Pos = Vector3.new(-1292,8,-3339)},
+    
+    -- More progression
+    {Level = 175, Name = "Military Soldier", Quest = "MagmaQuest", Mob = "Military Soldier", Pos = Vector3.new(-5408,11,8468)},
+    {Level = 190, Name = "Military Spy", Quest = "MagmaQuest", Mob = "Military Spy", Pos = Vector3.new(-5821,84,8820)},
+    {Level = 210, Name = "Fishman Warrior", Quest = "FishmanQuest", Mob = "Fishman Warrior", Pos = Vector3.new(61122,19,1515)},
+    {Level = 225, Name = "Fishman Commando", Quest = "FishmanQuest", Mob = "Fishman Commando", Pos = Vector3.new(61792,19,920)},
+    {Level = 250, Name = "Warden", Quest = "ImpelQuest", Mob = "Warden", Pos = Vector3.new(5278,0,927)},
+    {Level = 275, Name = "Chief Warden", Quest = "ImpelQuest", Mob = "Chief Warden", Pos = Vector3.new(5278,0,927)},
+    {Level = 300, Name = "Swan", Quest = "ImpelQuest", Mob = "Swan", Pos = Vector3.new(5332,0,925)},
+    
+    {Level = 325, Name = "Marine Lieutenant", Quest = "MarineQuest3", Mob = "Marine Lieutenant", Pos = Vector3.new(-2780,16,5600)},
+    {Level = 375, Name = "Marine Captain", Quest = "MarineQuest3", Mob = "Marine Captain", Pos = Vector3.new(-2500,8,6350)},
+    
+    -- Skypiea
+    {Level = 450, Name = "God's Guard", Quest = "SkyExp1Quest", Mob = "God's Guard", Pos = Vector3.new(-4720,843,4320)},
+    {Level = 475, Name = "Shanda", Quest = "SkyExp1Quest", Mob = "Shanda", Pos = Vector3.new(-4810,843,4370)},
+    {Level = 525, Name = "Royal Squad", Quest = "SkyExp2Quest", Mob = "Royal Squad", Pos = Vector3.new(-7900,563,5560)},
+    {Level = 550, Name = "Royal Soldier", Quest = "SkyExp2Quest", Mob = "Royal Soldier", Pos = Vector3.new(-8000,563,5700)},
+    
+    {Level = 575, Name = "Galley Pirate", Quest = "FountainQuest", Mob = "Galley Pirate", Pos = Vector3.new(5545,37,3992)},
+    {Level = 625, Name = "Galley Captain", Quest = "FountainQuest", Mob = "Galley Captain", Pos = Vector3.new(5580,37,4936)},
 }
