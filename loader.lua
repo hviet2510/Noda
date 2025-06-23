0a2f0d95d712be6f0c2b0db2fd57759871501817
hviet2510 <vieth1393@gmail.com>
2025-06-24 02:18:53 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,21 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/ui.lua"))()
-local EnemyList = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/EnemyList.lua"))()
-local AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/AutoFarm.lua"))()
-local AutoStats = loadstring(game:HttpGet("https://raw.githubusercontent.com/vieth2510/noda/main/modules/AutoStats.lua"))()
+local function loadModule(url)
+	local success, result = pcall(function()
+		return loadstring(game:HttpGet(url))()
+	end)
+	if not success then
+		warn("❗ Lỗi tải module:", url, "\n", result)
+	end
+	return result
+end
 
-AutoFarm(EnemyList)
-AutoStats()
+local baseURL = "https://raw.githubusercontent.com/hviet2510/Noda/main/modules/"
+
+local EnemyList = loadModule(baseURL.."enemylist.lua")
+local Config = loadModule(baseURL.."config.lua")
+local Tabs = loadModule(baseURL.."tabs.lua")
+local Buttons = loadModule(baseURL.."buttons.lua")
+local UI = loadModule(baseURL.."ui.lua")
+local AutoStats = loadModule(baseURL.."autostats.lua")
+local AutoFarm = loadModule(baseURL.."autofarm.lua")
+
+if UI then UI(Tabs, Buttons, Config, AutoFarm, EnemyList, AutoStats) end
