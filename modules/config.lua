ddd369efc0977c18a754240352892514c8d91b33
hviet2510 <vieth1393@gmail.com>
2025-06-24 11:09:30 +0700

    Update config.lua

--- modules/config.lua
+++ modules/config.lua
@@ -1,8 +1,8 @@
 local Config = {}
 
-Config.BringMob = true         -- true/false bật/tắt bring quái
-Config.FastAttack = true       -- true/false bật/tắt fast attack
-Config.AttackDelay = 0.1       -- Thời gian delay giữa các đòn đánh
-Config.PreferMelee = true      -- Ưu tiên trang bị và nâng melee
+Config.BringMob = true
+Config.FastAttack = true
+Config.AttackDelay = 0.1
+Config.PreferMelee = true
 
 return Config
