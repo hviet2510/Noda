a4af8b071f81f0d0d82bac0d60e12d540ed88be7
hviet2510 <vieth1393@gmail.com>
2025-06-24 11:18:57 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,22 +1,3 @@
-local function Notify(text)
-	print("[Loader] " .. text)
-end
-
-local function ImportModule(name)
-	Notify("Đang tải module: " .. name)
-	local success, result = pcall(function()
-		return loadstring(game:HttpGet("https://raw.githubusercontent.com/hviet2510/Noda/main/modules/" .. name .. ".lua"))()
-	end)
-	if success then
-		Notify("Đã tải xong module: " .. name)
-		return result
-	else
-		warn("[Loader ERROR] Module lỗi: " .. name .. "\n" .. tostring(result))
-	end
-end
-
-Notify("Bắt đầu tải các module...")
-
 local Tabs = ImportModule("tabs")
 local Buttons = ImportModule("buttons")
 local Config = ImportModule("config")
@@ -26,9 +7,5 @@ local AutoStats = ImportModule("autostats")
 local UI = ImportModule("ui")
 
 if UI then
-	Notify("Khởi tạo UI...")
 	UI(Tabs, Buttons, Config, AutoFarm, EnemyList, AutoStats)
-	Notify("Hoàn tất.")
-else
-	warn("[Loader ERROR] Không thể khởi tạo UI!")
 end
